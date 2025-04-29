bce7624d778a72c6ea06b0aed94b11661261d2fb
muhiddinov <muhiddinovabulqosim@gmail.com>
2025-04-29 19:19:09 +0500

    fixing

--- carwash/process.py
+++ carwash/process.py
@@ -16,6 +16,7 @@ class ProcessWindow(QWidget):
         self.pause_clicked = False
         self.pause_time = 3 * 60 # 3 daqiqa
         self.work_time_in_second = 0
+        self.active_pin = 0
         self.discounting = 0
         self.current_time = QTime.currentTime().toString("hh:mm")
         
@@ -28,17 +29,18 @@ class ProcessWindow(QWidget):
         # GPIO sozlamalari
         GPIO.setmode(GPIO.BCM)
         self.output_pins = [relay["port"] for relay in self.relays]
-        self.input_pins = [3, 18, 23, 24, 25, 8, 7, 12, 16, 20, 21]
-        self.interrupt_pin = 21  # Interrupt uchun kiruvchi pin
+        self.input_pins = [2, 3, 18, 23, 24, 25, 8, 7, 12, 16]
+        self.cash_interrupt_pin = 21  # Interrupt uchun kiruvchi pin
         self.summa = 0  # Umumiy summa
         if os.path.exists('last.a'):
             with open('last.a', 'r') as last:
                 self.summa = int(last.read())
             
         self.out_pwr_en = 17
-        self.pause_pin = 2
+        self.pause_interrupt_pin = 20
         
         GPIO.setup(self.out_pwr_en, GPIO.OUT)
+        GPIO.output(self.out_pwr_en, GPIO.HIGH)
         
         # Chiquvchi pinlarni sozlash
         for pin in self.output_pins:
@@ -51,18 +53,11 @@ class ProcessWindow(QWidget):
             GPIO.add_event_detect(pin, GPIO.RISING, callback=self.execute_task, bouncetime=200)
 
         # Interrupt pinni sozlash
-        GPIO.setup(self.pause_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-        GPIO.add_event_detect(self.pause_pin, GPIO.RISING, callback=self.pause_callback, bouncetime=200)
-        GPIO.setup(self.interrupt_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-        GPIO.add_event_detect(self.interrupt_pin, GPIO.FALLING, callback=self.handle_interrupt, bouncetime=50)
+        GPIO.setup(self.pause_interrupt_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.add_event_detect(self.pause_interrupt_pin, GPIO.FALLING, callback=self.pause_callback, bouncetime=200)
+        GPIO.setup(self.cash_interrupt_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        GPIO.add_event_detect(self.cash_interrupt_pin, GPIO.FALLING, callback=self.handle_interrupt, bouncetime=50)
 
-        # Pixel shriftni yuklash
-        # font_id = QFontDatabase.addApplicationFont("assets/pixel_lcd_7.ttf")
-        # if font_id == -1:
-        #     print("Shrift yuklanmadi. .ttf fayl mavjudligini tekshiring.")
-        #     sys.exit(1)
-        # font_family = QFontDatabase.applicationFontFamilies(font_id)[0]
-        # font = QFont(font_family)
         font = QFont("FreeSerif")
         font.setPixelSize(400)
 
@@ -110,9 +105,13 @@ class ProcessWindow(QWidget):
         self.lbl_timer.setGeometry(0, 0, size.width(), size.height() // 2)
         self.lbl_value.setGeometry(0, size.height() // 2, size.width(), size.height() // 2)
             
-    def pause_callback(self):
-        self.pause_clicked = True
-        self.lbl_func.setText("PAUSE")
+    def pause_callback(self, index):
+        print(f"Pause Callback: {index}")
+        if self.work_time_in_second > 0:
+            self.pause_clicked = True
+            self.lbl_func.setText("PAUSE")
+            for pin in self.output_pins:
+                GPIO.output(pin, GPIO.LOW)
             
     def update_time(self):
         minute = int(self.work_time_in_second / 60)
@@ -127,42 +126,61 @@ class ProcessWindow(QWidget):
             second = int(self.work_time_in_second % 60)
             if self.work_time_in_second > 0:
                 self.work_time_in_second -= 1
-            
+                if self.active_pin > 0:
+                    GPIO.output(self.active_pin, GPIO.LOW)
+                    if self.summa > 0:
+                        self.summa -= self.discounting
+                        if self.summa < 0:
+                            self.summa = 0
+                        self.lbl_value.setText(str(self.summa) + " so'm")
+                        with open('last.a', 'w') as last_file:
+                            last_file.write(str(self.summa))
+            else:
+                self.summa = 0
+                self.work_time_in_second = 0
+                self.lbl_func.setText("----")
+                if self.summa <= 0:
+                    self.lbl_value.setText("----")
+                
+                    
         if minute > 0 or second > 0:
-            self.current_time = str(f"{minute}:{second}")
+            self.current_time = str(f"{minute:02}:{second:02}")
         else:
             self.current_time = QTime.currentTime().toString("hh:mm")
+            self.lbl_func.setText("----")
         if self.toggle_clock:
             self.current_time  = self.current_time.replace(":", " ")
         self.toggle_clock = not self.toggle_clock
         self.lbl_timer.setText(self.current_time)
-        if self.summa > 0:
-            with open('last.a', 'w') as last_file:
-                last_file.write(str(self.summa))
-            self.summa -= self.discounting
-            self.lbl_value.setText(str(self.summa))
         
     def handle_interrupt(self, channel):
         self.summa += 1000
-        self.lbl_value.setText(f"{self.summa}")
+        self.
+        self.lbl_value.setText(f"{self.summa} so'm")
 
     def execute_task(self, task_index):
+        self.pause_clicked = False
         if self.summa > 0:
+            for pin in self.output_pins:
+                GPIO.output(pin, GPIO.LOW)
             print(f"Tugma {task_index} bosildi. Vazifa bajarilmoqda...")
             input_index = self.input_pins.index(task_index)
-            GPIO.output(self.output_pins[input_index], GPIO.HIGH)
+            self.active_pin = self.output_pins[input_index]
+            GPIO.output(self.active_pin, GPIO.HIGH)
             price = self.relays[input_index]["price"]
             funk_name = self.relays[input_index]["desc"]
             self.lbl_func.setText(funk_name)
             self.work_time_in_second = self.summa * 60 / price
-            self.discounting = price / 60
+            self.discounting = int(price / 60)
         else:
+            self.active_pin = 0
             self.lbl_value.setText("Pul yetarli emas")
             QTimer.singleShot(2000, lambda: self.lbl_value.setText("----"))
-        self.pause_clicked = False
         
         
     def closeEvent(self, event):
+        for pin in self.output_pins:
+            GPIO.output(pin, GPIO.LOW)
         GPIO.cleanup()
         event.accept()
 
