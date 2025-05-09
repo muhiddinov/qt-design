74ef8ee8e9177e737816fcbdfdd6df4d11a92e70
Abulqosim Muhiddinov <muhiddinovabulqosim@gmail.com>
2025-05-10 02:43:17 +0500

    process

--- carwash/process.py
+++ carwash/process.py
@@ -153,22 +153,23 @@ class ProcessWindow(QWidget):
         if self.timer_counter >= 5:
             self.timer_counter = 0
             self.toggle_clock = not self.toggle_clock
+            
         if self.in_option == False:
             if self.pause_clicked:
                 if self.cash_sum <= 0:
                     self.pause_clicked = False
-                    return
                 lbl_func_text = "PAUSE"
                 self.pause_time -= 0.1
                 if self.pause_time <= 0:
                     self.pause_clicked = False
                     self.pause_time = 0
                     if self.cash_sum > 0:
-                        self.in_option = True
-                        self.option_time = self.cash_sum * 60 / self.penalty_time_cost
-                        self.cash_sum_discount = self.cash_sum / self.penalty_time_cost / 10
-                        self.cash_data_post = False
-                        self.last_option['name'] = "JARIMA"
+                        if self.last_option['name'] != "JARIMA":
+                            self.in_option = True
+                            self.option_time = self.cash_sum * 60 / self.penalty_time_cost
+                            self.cash_sum_discount = self.cash_sum / self.penalty_time_cost / 10
+                            self.cash_data_post = False
+                            self.last_option['name'] = "JARIMA"
                 lbl_timer_text = self.seconds_to_str(int(self.pause_time), "%M:%S") if self.toggle_clock else self.seconds_to_str(int(self.pause_time), "%M %S")
             else:
                 self.pause_time = self.config.pause_time
