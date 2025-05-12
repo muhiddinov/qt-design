4be6187cde44c162c9ff0e9f4f3242abe4a38d90
muhiddinov <muhiddinovabulqosim@gmail.com>
2025-05-12 17:13:09 +0500

    updated

--- carwash/process.py
+++ carwash/process.py
@@ -167,8 +167,7 @@ class ProcessWindow(QWidget):
                     '--no-sandbox',
                     '--kiosk',
                     'http://127.0.0.1'])
-                time.sleep(3)
-                self.hide()
+                # self.hide()
                 self.browser_opened = True
             
         if self.in_option == False:
